4045786c4ef6b36006b0e14ccf662e775410c1b4
Andrew Smith <topcodeandrew@gmail.com>
2020-11-18 16:31:19 -0700

    app5 DONE

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Image from './Components/Image'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Image source={'https://i.insider.com/5c799c56eb3ce834ad57b632?width=750&format=jpeg&auto=webp'} />
     </div>
   );
 }
